d199f2bdfc836d1506316a3937ff49a92b5b524a
Ömer Can Demirel <1902009012@student.beykent.edu.tr>
2023-12-28 18:01:09 +0300

    commitoglu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageOneComponent } from './page-one/page-one.component';
+import { PageTwoComponent } from './page-two/page-two.component';
+import { PostComponent } from '../components/post/post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", component:HomeComponent},
+    {path:"pageOne", component:PageOneComponent},
+    {path:"pageTwo", component:PageTwoComponent},
+    {path:"posts", component:PostComponent},
+    {path:"users", component:UserComponent}
+];
